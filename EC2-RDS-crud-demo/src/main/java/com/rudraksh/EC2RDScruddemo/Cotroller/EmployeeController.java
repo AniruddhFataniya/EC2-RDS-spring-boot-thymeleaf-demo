64c773accc6e2e0be7ffd6bdbc9d8cb7d59a1fe4
Admin <fataniya.ag@gmail.com>
2022-07-11 10:03:58 +0530

    First commit

---
+++ EC2-RDS-crud-demo/src/main/java/com/rudraksh/EC2RDScruddemo/Cotroller/EmployeeController.java
@@ -0,0 +1,52 @@
+package com.rudraksh.EC2RDScruddemo.Cotroller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.rudraksh.EC2RDScruddemo.Model.Employee;
+import com.rudraksh.EC2RDScruddemo.repository.EmployeeRepository;
+
+@Controller
+public class EmployeeController {
+	
+	@Autowired
+	public EmployeeRepository employeeRepository;
+	
+	//Employee employee;
+	
+	//Display Employee list 
+	@GetMapping("/")
+	public String showEmployee(Model model) {
+		
+		List<Employee> employeeList = employeeRepository.findAll();
+		
+		model.addAttribute("employee",employeeList);
+		
+		return "index";
+	}	
+	
+	@GetMapping("/employeeForm")
+	public String showEmployeeForm(Model model) {
+		
+		Employee theEmployee = new Employee();
+		model.addAttribute("employee",theEmployee);
+		return "employee-form";
+	}
+	
+	
+	@PostMapping("/addEmployee")
+	public String addEmployee(@ModelAttribute Employee employee,Model model) {
+		//System.out.println(employee);
+		model.addAttribute("employee",employee);
+		employeeRepository.save(employee);
+		return "redirect:/";
+	}
+	
+
+}
---
+++ EC2-RDS-crud-demo/src/test/java/com/rudraksh/EC2RDScruddemo/Ec2RdsCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudraksh.EC2RDScruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2RdsCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
